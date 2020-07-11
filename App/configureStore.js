95498bbfb07db632b4e591bb9dbfc377af8e40ff
Gopinath C <gopinath@devkyyba.com>
2020-07-11 14:41:10 +0530

    small tweaks

--- App/configureStore.js
+++ App/configureStore.js
@@ -1,23 +1,3 @@
-// import { createStore, applyMiddleware } from 'redux';
-// import createSagaMiddleware from 'redux-saga';
-// import { composeWithDevTools } from 'redux-devtools-extension';
-// import logger from 'redux-logger';
-// import rootWatchers from './sagas';
-// import rootReducer from './reducers';
-
-
-// const sagaMiddleware = createSagaMiddleware();
-// const composeEnhancers = composeWithDevTools({});
-// export default function configureStore(preloadedState = {}) {
-//   const store = createStore(
-//     rootReducer,
-//     preloadedState,
-//     composeEnhancers(applyMiddleware(sagaMiddleware, logger)),
-//   );
-//   sagaMiddleware.run(rootWatchers);
-//   return store;
-// }
-
 import { applyMiddleware, compose, createStore } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import { persistReducer, persistStore } from 'redux-persist'
